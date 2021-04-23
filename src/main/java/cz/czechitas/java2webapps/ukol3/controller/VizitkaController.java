8b54652491a96c4fbe8d935d8e5db4b76c0eea67
lang-do <do.langova@gmail.com>
2021-04-23 10:31:51 +0200

    Vizitkovník

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,38 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
+  private final List<Vizitka> vizitky;
+  public VizitkaController() {
+    vizitky = List.of(
+         new Vizitka("Dominika Langová", "Proton Therapy Center Czech", "Budínova 2437/1a", "Praha 8, 180 00", "Budínova 2437/1a, 180 00 Praha 8", "dominika.langova@ptc.cz", "222 999 000", "ptc.cz"),
+         new Vizitka("Veronika Mlejnková", "Proton Therapy Center Czech", "Budínova 2437/1a", "Praha 8, 180 00", "Budínova 2437/1a, 180 00 Praha 8", "veronika.mlejnkova@ptc.cz", "222 999 000", "ptc.cz"),
+         new Vizitka("zastupující kolega", "Proton Therapy Center Czech", "Budínova 2437/1a", "Praha 8, 180 00", "Budínova 2437/1a, 180 00 Praha 8", "", "222 999 000", "ptc.cz")
+    );
+  }
+
+
   @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+  public ModelAndView seznam() {
+    ModelAndView modelAndView = new ModelAndView("seznam");
+    modelAndView.addObject("Vizitka", vizitky);
+    return modelAndView;
   }
 
   @GetMapping("/detail")
-  public String detail() {
-    return "detail";
+  public ModelAndView detail(int id) {
+    ModelAndView modelAndView = new ModelAndView("detail");
+    modelAndView.addObject("Vizitka", vizitky.get(id));
+    return modelAndView;
   }
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
@@ -0,0 +1,88 @@
+package cz.czechitas.java2webapps.ukol3.entity;
+
+public class Vizitka {
+
+    private String jmeno;
+    private String firma;
+    private String ulice;
+    private String obecPsc;
+    private String celaAdresa; //(readonly property, ve tvaru „ulice, obecPsc“)
+    private String email;
+    private String telefon;
+    private String web;
+
+    public Vizitka(String jmeno, String firma, String ulice, String obecPsc, String celaAdresa, String email, String telefon, String web) {
+        this.jmeno = jmeno;
+        this.firma = firma;
+        this.ulice = ulice;
+        this.obecPsc = obecPsc;
+        this.celaAdresa = celaAdresa;
+        this.email = email;
+        this.telefon = telefon;
+        this.web = web;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public String getObecPsc() {
+        return obecPsc;
+    }
+
+    public String getCelaAdresa() {
+        return ulice + "," + obecPsc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public void setObecPsc(String obecPsc) {
+        this.obecPsc = obecPsc;
+    }
+
+    /*public void setCelaAdresa(String celaAdresa) {
+        this.celaAdresa = celaAdresa;                   //read only
+    }*/
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+}
